e463c1c48fa447782151d2d78ba63606e8384382
Paula Ribeiro <paula-mr@users.noreply.github.com>
2017-08-10 09:45:34 -0300

    corrige instância de socket

--- src/cefetmg/inf/server/Servidor.java
+++ src/cefetmg/inf/server/Servidor.java
@@ -17,22 +17,29 @@ public class Servidor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        while (true) {
-            try {
-                ServerSocket s = new ServerSocket(2223);
-                System.out.println("Aguardando conexão");
+        
+        try {
+            ServerSocket s;
+
+            while (true) {
                 
+                s = new ServerSocket(2223);
+                
+                System.out.println("Aguardando conexão");
+
                 Socket p = s.accept();
                 System.out.println("Conexão estabelecida");
-                
+
                 AdaptadorCalculadora adapter = new AdaptadorCalculadora(p);
                 new Thread(adapter).start();
-                
+
                 s.close();
-            } catch (Exception e) {
-                System.out.println("Ocorreu um erro! \n" + e.getMessage());
             }
+            
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro! \n" + e.getMessage());
         }
+        
     }
     
 }
